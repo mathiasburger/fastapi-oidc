784edcd4d3e87ce45588581f557ab21471fd2adf
Mathias Burger <mathias.burger@tngtech.com>
2022-11-12 22:36:49 +0100

    Comment that offline_access requires user consent

--- angular_client/src/app/auth/auth-config.module.ts
+++ angular_client/src/app/auth/auth-config.module.ts
@@ -10,12 +10,13 @@ import {AuthModule, LogLevel} from 'angular-auth-oidc-client';
       redirectUrl: window.location.origin,
       postLogoutRedirectUri: window.location.origin,
       clientId: 'myclient', // client id
-      scope: 'openid profile offline_access', // 'openid profile offline_access ' + your scopes
+      scope: 'openid profile', // 'offline_access' scope requires user consent which needs to be enabled in auth server
       responseType: 'code',
       silentRenew: true,
       useRefreshToken: true,
       renewTimeBeforeTokenExpiresInSeconds: 30,
       logLevel: LogLevel.Debug,
+      ignoreNonceAfterRefresh: true,
     }
   })],
   exports: [AuthModule],
